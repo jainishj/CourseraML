e934de6894ad9d98fffd2c6ace2f01a5b867baf6
Jainish Jain <jainish.jain@autodesk.com>
2019-08-21 00:33:24 +0530

    ex 2 solutions

--- LogisticRegression/Ex-Solution/ex2/costFunction.m
+++ LogisticRegression/Ex-Solution/ex2/costFunction.m
@@ -8,7 +8,6 @@ function [J, grad] = costFunction(theta, X, y)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
 grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
@@ -20,12 +19,8 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
-
-
-
-
-
-
+J = (-(y'*log(sigmoid(X*theta))) - ((1-y)'*log(1-sigmoid(X*theta))))/m;
+grad = (X'*(sigmoid(X*theta) - y))/m;
 
 % =============================================================
 
--- LogisticRegression/Ex-Solution/ex2/costFunctionReg.m
+++ LogisticRegression/Ex-Solution/ex2/costFunctionReg.m
@@ -17,10 +17,10 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+J = (-(y'*log(sigmoid(X*theta))) - ((1-y)'*log(1-sigmoid(X*theta))))/m + lambda/(2*m)*sum(theta(2:end).^2);
 
-
-
-
+grad = (X'*(sigmoid(X*theta) - y))/m + lambda/m*theta;
+grad(1) = sum(sigmoid(X*theta)-y)/m;
 
 % =============================================================
 
